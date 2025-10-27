1eb7f3cf4aabc16651fbd3fe1a49dfa9998cf80b
Rakibul Islam <rakibulislammondal21@gmail.com>
2025-10-27 23:38:27 +0530

    Added Binary search varients

--- Array ADT/BinarySearch-Varients/02_Last_Occurance.cpp
+++ Array ADT/BinarySearch-Varients/02_Last_Occurance.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
+
 struct Array {
     int A[20];
-    int size;
+    int size ;
     int length;
 };
 
-void Display(Array arr);
-int FirstOccurance(Array arr, int target);
+void Display (Array arr);
+int LastOccurance(Array arr, int target);
+
 
 int main (){
     struct Array arr = {
@@ -24,36 +26,36 @@ int main (){
     int target;
     cin >> target;
 
-    int index = FirstOccurance(arr,target);
+    int index = LastOccurance(arr,target);
 
     if(index == -1){
         cout << "Not Found!\n";
         return 0;
     }
 
-    cout << "The Number " << target << " is at index (First occurance) : " << index << endl;
+    cout << "The Number " << target << " is at index (Last occurance) : " << index << endl;
 
 
     return 0;
 }
 
-int FirstOccurance(Array arr, int target){
+int LastOccurance(Array arr, int target){
     int low = 0;
-    int high = arr.length -1;
-
+    int high = arr.length -1 ;
     int index = -1;
 
-    while(low <= high){
-        int mid = low + (high - low)/2;
-        
-        if(arr.A[mid] == target) {
-            index = mid; // store mid as probable answer
-            high = mid - 1; //continue searching left ;
+    while (low<=high)
+    {   
+        int mid = low + (high-low)/2;
+        if(arr.A[mid] == target){
+            index = mid;
+            low = mid + 1; // continue searching right
         }
         else if(arr.A[mid] > target) high = mid -1;
-        else low = mid + 1 ;
+        else low = mid + 1;
     }
     return index;
+    
 }
 
 void Display(Array arr) {
